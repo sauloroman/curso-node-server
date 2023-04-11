c99da5d05a23a78b1fda031f4c97b252a091813c
Saulo Roman Santillan Nava <romansantillan1998@outlook.com>
2023-04-10 19:17:56 -0600

    Fin seccion 11 -version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const User = require("../models/User")
 const { generateJWT } = require("../helpers/generate-jwt")
+const { googleVerify } = require('../helpers/google-verify')
 
 const login = async ( req, res ) => {
   
@@ -48,6 +49,57 @@ const login = async ( req, res ) => {
   
 }
 
+const googleSignIn = async ( req, res = response ) => {
+
+  const { id_token } = req.body
+
+  try {
+
+    const { name, email, picture } = await googleVerify( id_token )
+
+    let user = await User.findOne({ email })
+
+    if ( !user ) {
+      // Crearlo
+      const data = {
+        username: name,
+        email,
+        password: 'Password no neccesary',
+        img: picture,
+        google: true,
+        role: 'USER_ROLE'
+      }
+
+      user = new User( data )
+      await user.save()
+
+    }
+
+    // Si el usuario en DB 
+    if ( !user.isActive ) {
+      return res.status(401).json({
+        msg: 'Talk to administrator. User blocked'
+      })
+    }
+
+    // Generar JWT
+    const token = await generateJWT( user._id )
+
+    res.json({
+     user,
+     token
+    })
+
+  } catch (error) {
+    console.log( error )
+    res.status(400).json({
+      msg: 'Token was not verified'
+    })
+  }
+  
+}
+
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
